dd83016b8280c3b56f8030fdc24dd1b0dc0e57c0
user <wpelletier.development@yahoo.com>
2023-05-31 20:59:59 -0400

    add the base springboot project

---
+++ webapp/src/main/java/com/vireo/vireo/VireoApplication.java
@@ -0,0 +1,13 @@
+package com.vireo.vireo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VireoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VireoApplication.class, args);
+	}
+
+}
