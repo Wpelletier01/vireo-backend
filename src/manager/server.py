7413ee3157d20d2c1bfb590e8c0ef5ecfd74935f
user <wpelletier.development@yahoo.com>
2023-06-09 23:41:19 -0400

    residing api and remove not needed file

--- src/manager/server.py
+++ src/manager/server.py
@@ -1,16 +1,23 @@
-import os.path
-
 from src.database.client import DbClient
 from src.database.container import Channel
 
 # from src.manager.upload import UploadManager
 from src.manager.error import VireoError, ErrorType
 from datetime import datetime, timedelta
-from dataclasses import dataclass
+from deffcode import FFdecoder
+from functools import wraps
+
 import logging
 import jwt
 import bcrypt
 import configparser
+import os
+import random
+import string
+import cv2
+import shutil
+
+# TODO: Make that when vireo exception is raised than log them automatically
 
 SUCCESS = 200
 BAD_REQUEST = 400
@@ -31,6 +38,19 @@ SIGNUP_KEY = [
 ]
 
 
+def _validate_body(body: dict, keys: list) -> bool:
+    for key in keys:
+        found = False
+
+        if key in body.keys():
+            found = True
+
+        if not found:
+            return False
+
+    return True
+
+
 # TODO: use join table query for getting username of the channel with videos
 
 class Server:
@@ -41,33 +61,24 @@ class Server:
         self.__config = configparser.ConfigParser()
         self.__config.read("config.ini")
 
+        self.__data_dir = self.__config['DEVELOPMENT']['db-dir']
+
         # TODO: implement when not in development
-        logfile = os.path.join(self.__config['DEVELOPMENT']['db-dir'], "app.log")
+        logfile = os.path.join(self.__data_dir, "app.log")
 
         self.db_client = DbClient(dict(self.__config["DATABASE"]))
         self.db_client.initiate_connection()
 
         self.__secret = self.__config["TOKEN"]["secret"]
         self.__alg = self.__config["TOKEN"]["algorithm"]
-        print(self.__alg)
 
-    def process_token(self, header: dict, ipaddr: str) -> (dict, int):
+    def __process_token(self, header: dict, ipaddr: str) -> (dict, int):
 
         try:
-            auth = header["Authorization"]
+            token = header.get("Vireo-Token")
 
         except KeyError:
-            logging.debug(f"{ipaddr} - request missing authorization header field")
-            return {}, BAD_REQUEST
-
-        # eg. authorization: Basic Tokekkekdkskakakdfkaskdfkkas
-        auth = auth.split(" ")
-
-        if len(auth) != 2:
-            logging.debug(f"{ipaddr} - malformed authorization header field")
-            return {}, BAD_REQUEST
-
-        token = auth[1]
+            return {"response": "token"}, BAD_REQUEST
 
         try:
             payload = jwt.decode(token, key=self.__secret, algorithms=self.__alg)
@@ -80,27 +91,108 @@ class Server:
         password = payload["password"]
 
         try:
-            hpassword = self.db_client.query(f"""
+            hpasswd = self.db_client.query(f"""
             SELECT Password FROM Channels WHERE USERNAME = '{username}';""")[0][0]
 
         except VireoError as e:
             logging.error(f"{ipaddr} - {e}")
-            return {}, INTERNAL_ERROR
+            return {"response": ""}, INTERNAL_ERROR
 
-        return SUCCESS
+        except IndexError:
+            logging.error(f"{ipaddr} - have jwt but his username dont exist")
+            return {"response": "bad-token-username"}, BAD_REQUEST
+
+        if not bcrypt.checkpw(password, hpasswd):
+            return {"response": "bad-token-password"}, BAD_REQUEST
+
+        return {}, SUCCESS
+
+    def __gen_token(self, username: str, hpassword: str) -> str:
+        expire = (datetime.now() + timedelta(hours=3)).timestamp()
+
+        payload = {
+            "username": username,
+            "password": hpassword,
+            "exp": expire
+        }
+
+        return jwt.encode(payload, self.__secret, algorithm=self.__alg)
+
+    def __query_video(self, squery: str) -> (dict, int):
+        query = f"""
+            SELECT v.Title, c.Username, v.PathHash
+            FROM Videos v
+            JOIN Channels c
+            ON v.ChannelID = c.ChannelID
+            WHERE
+                Title like '%{squery}%' OR
+                Description like '%{squery}%';"""
+
+        try:
+            videos = self.db_client.query(query)
+        except VireoError as e:
+            logging.error(f"database call failed: {e}")
+            return {"response": ""}, INTERNAL_ERROR
+
+        response = []
+        for video in videos:
+            info = {
+                "type":         "video",
+                "title":        video[0],
+                "channel":      video[1],
+                "thumbnail":    video[2]
+            }
+
+            response.append(info)
+
+        return {"response": response}, SUCCESS
+
+    def __query_channel(self, squery: str) -> (dict, int):
+        # TODO: add query for the number of video that each channels have
+
+        try:
+            channels = self.db_client.query(f"""
+                SELECT Username
+                FROM Channels
+                WHERE Username like '%{squery}%';""")
+        except VireoError as e:
+            logging.error(f"database call failed: {e}")
+            return {"response": ""}, INTERNAL_ERROR
+
+        response = []
+
+        for channel in channels:
+            info = {
+                "type": "channel",
+                "channel": channel[0],
+                "title": None,
+                "hpath": None
+
+            }
+            response.append(info)
+
+        return {"response": response}, SUCCESS
+
+    def __create_thumbnail(self, tmp_fp: str, hash_path: str):
+
+        # TODO: check for error
+        # TODO: resize for best thumbnails
+        video = cv2.VideoCapture(tmp_fp)
+
+        frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
+        frame_id = random.randint(0, frames)
+
+        video.set(cv2.CAP_PROP_POS_FRAMES, frame_id)
+
+        success, frame = video.read()
+
+        cv2.imwrite(os.path.join(self.__data_dir, f"thumbnails/{hash_path}.png"), frame)
 
     def handle_signin(self, data, ipaddr: str) -> (dict, int):
 
         # validate body
-        for key in ["username", "password"]:
-            found = False
-
-            if key in data.keys():
-                found = True
-
-            if not found:
-                logging.debug(f"{ipaddr} - malformed body field")
-                return {}, BAD_REQUEST
+        if not _validate_body(data, ["username", "password"]):
+            return {"response": ""}, BAD_REQUEST
 
         try:
             username, hpassword = self.db_client.query(f"""
@@ -110,39 +202,24 @@ class Server:
 
         except VireoError as e:
             logging.error(e)
-            return {}, INTERNAL_ERROR
+            return {"response": ""}, INTERNAL_ERROR
         except IndexError as e:
             return {"response": "username"}, UNAUTHORIZED
 
         if not bcrypt.checkpw(data["password"].encode('utf-8'), hpassword.encode('utf-8')):
             return {"response": "password"}, UNAUTHORIZED
 
-        expire = (datetime.now() + timedelta(hours=3)).timestamp()
-
-        payload = {
-            "username": username,
-            "password": hpassword,
-            "exp": expire
-        }
-
-        token = jwt.encode(payload, self.__secret, algorithm=self.__alg)
-
-        return {"token": token}, 200
+        return {
+            "response": {"token": self.__gen_token(username, hpassword)}
+        }, SUCCESS
 
     def handle_sign_up(self, data: dict, ipaddr: str) -> (dict, int):
 
-        for key in SIGNUP_KEY:
-            found = False
-
-            if key in data.keys():
-                found = True
-
-            if not found:
-                return {"response": ""}, BAD_REQUEST
+        if not _validate_body(data, SIGNUP_KEY):
+            return {"response": ""}, BAD_REQUEST
 
         valid_username = False
         try:
-
             user = self.db_client.query(f"""
                 SELECT Username
                 FROM Channels
@@ -150,7 +227,7 @@ class Server:
 
         except VireoError as e:
             logging.error(f"{ipaddr} - {e}")
-            return {}, 500
+            return {"response": ""}, 500
         except IndexError:
             valid_username = True
 
@@ -166,7 +243,7 @@ class Server:
 
         except VireoError as e:
             logging.error(f"{ipaddr} - {e}")
-            return {}, 500
+            return {"response": ""}, 500
         except IndexError:
             valid_email = True
 
@@ -185,7 +262,7 @@ class Server:
 
         except VireoError as e:
             logging.error(f"{ipaddr} - {e}")
-            return {}, INTERNAL_ERROR
+            return {"response": ""}, INTERNAL_ERROR
 
         insert1 = f"""
             INSERT INTO Channels (
@@ -201,7 +278,7 @@ class Server:
 
         except VireoError as e:
             logging.error(f"{ipaddr} - {e}")
-            return {}, INTERNAL_ERROR
+            return {"response": ""}, INTERNAL_ERROR
 
         insert2 = f"""
             INSERT INTO ChannelDetails (
@@ -220,5 +297,203 @@ class Server:
                 '{data["lname"]}',
                 '{data["email"]}',
                 Date('{birthday.isoformat()}'));"""
+        try:
+            self.db_client.insert(insert2)
 
-        return {}, 200
+        except VireoError as e:
+            logging.error(f"{ipaddr} - {e}")
+            return {"response": ""}, INTERNAL_ERROR
+
+        return {"response": "success"}, 200
+
+    def handle_upload(self, data: bytes, header: dict, ipaddr: str) -> (dict, int):
+        # TODO: validate size
+        # TODO: make sure that the user cant upload more than one video at time
+
+        payload, result = self.__process_token(header, ipaddr)
+
+        if result != SUCCESS:
+            return payload, result
+
+        tmp = os.path.join(self.__data_dir, f"tmp/{ipaddr}.mp4")
+
+        with open(tmp, "wb") as f:
+            f.write(data)
+            f.close()
+
+        decoder = FFdecoder(tmp)
+
+        try:
+            video_info = decoder.metadata["vireo"]
+
+        except KeyError:
+            return {"response": "vireo"}, BAD_REQUEST
+
+        if _validate_body(video_info, ["title", "description"]):
+            return {}, BAD_REQUEST
+
+        possibility = string.ascii_lowercase + string.ascii_lowercase + string.digits
+        hpath = "".join(random.choice(possibility) for c in range(7))
+
+        try:
+            channel_id = self.db_client.query(
+                f"""SELECT ChannelID FROM Channels WHERE Username = '{payload["username"]}';"""
+            )[0][0]
+        except VireoError as e:
+            logging.error(f"{ipaddr} - {e}")
+            return {"response": ""}, INTERNAL_ERROR
+
+        insert = f"""
+            INSERT INTO Videos(
+                PathHash,
+                ChannelID,
+                Title,
+                Description,
+                Upload
+            )
+            VALUE (
+                '{hpath}',
+                '{channel_id}',
+                '{video_info["title"]}',
+                '{video_info["description"]}',
+                Date('{datetime.now().timestamp()}')
+            );"""
+
+        try:
+            self.db_client.insert(insert)
+
+        except VireoError as e:
+            logging.error(f"{ipaddr} - {e}")
+            return {"response": ""}, BAD_REQUEST
+
+        self.__create_thumbnail(tmp, hpath)
+
+        shutil.copy(tmp, os.path.join(self.__data_dir, f"videos/{hpath}.mp4"))
+
+        return {"response": hpath}, SUCCESS
+
+    def retrieve_video_info(self, rtype: str, channel: str | None) -> (dict, str):
+
+        videos = []
+        info = []
+        query = ""
+
+        match rtype:
+            case "all":
+                query = f"""
+                    SELECT v.PathHash, v.Title, c.Username, v.Upload
+                    FROM Videos v
+                    JOIN Channels c
+                    ON v.ChannelID = c.ChannelID;"""
+
+            case "channel":
+                if channel is None:
+                    logging.error("try retrieve channel video but no username is passed")
+                    return {"response": ""}, INTERNAL_ERROR
+
+                query = f"""
+                    SELECT v.PathHash, v.Title, c.Username, v.Upload
+                    FROM Videos v
+                    JOIN Channels c
+                    ON v.ChannelID = c.ChannelID 
+                    WHERE c.Username = '{channel}';"""
+
+            case _:
+                return {"response": "bad-url-type"}, BAD_REQUEST
+
+        info = []
+
+        try:
+            videos = self.db_client.query(query)
+
+        except VireoError as e:
+            logging.error(f"database call failed: {e}")
+            return {}, INTERNAL_ERROR
+
+        for video in videos:
+            hpath = video[0]
+            date = video[3]
+            title = video[1]
+            nchannel = video[2]
+
+            info.append({
+                "thumbnail":    hpath,
+                "channel":      nchannel,
+                "title":        title,
+                "upload":       date
+            })
+
+        return {"response": info}, SUCCESS
+
+    def handle_search(self, squery: str, stype: str) -> (dict, str):
+
+        response = []
+        # TODO: add duration of info return
+        # TODO: make different type of query like for only channel
+        match stype:
+            case "all":
+                videos, status = self.__query_video(squery)
+
+                if status != SUCCESS:
+                    return videos, status
+
+                for video in videos["response"]:
+                    response.append(video)
+
+                channels, status = self.__query_channel(squery)
+
+                if status != SUCCESS:
+                    return channels, status
+
+                for channel in channels["response"]:
+                    response.append(channel)
+
+            case _:
+                return {"response": "bad-search-type"}, BAD_REQUEST
+
+        return {"response": response}, SUCCESS
+
+    def get_thumbnail_path(self, hpath: str) -> (dict, int):
+        thumbnails_dir = os.path.join(self.__data_dir, "thumbnails")
+
+        if f"{hpath}.png" not in os.listdir(thumbnails_dir):
+            return {"response": "no-thumbnail"}, BAD_REQUEST
+
+        return {"response": os.path.join(thumbnails_dir, f"{hpath}.png")}, SUCCESS
+
+    def get_video(self, hpath: str) -> (dict,):
+        videos_dir = os.path.join(self.__data_dir, "videos")
+
+        if f"{hpath}.mp4" not in os.listdir(videos_dir):
+            return {"response": "no-video"}, BAD_REQUEST
+
+        url = os.path.join(videos_dir, f"{hpath}.mp4")
+
+        return {"response": url}, SUCCESS
+
+    def get_vinfo(self, hpath: str):
+
+        query = f"""
+                    SELECT v.Title, c.Username,v.Description
+                    FROM Videos v
+                    JOIN Channels c
+                    ON v.ChannelID = c.ChannelID
+                    WHERE PathHash = '{hpath}';"""
+
+        try:
+            vinfo = self.db_client.query(query)[0]
+
+        except VireoError as e:
+            logging.error(f"database call failed: {e}")
+            return {"response": ""}, INTERNAL_ERROR
+        except IndexError:
+            logging.error(f"video file exist but not in the database")
+            return {"response": ""}, INTERNAL_ERROR
+
+        return {"response": {
+            "title": vinfo[0],
+            "channel": vinfo[1],
+            "description": vinfo[2],
+            "hpath":  hpath
+            }
+        }, SUCCESS
--- src/service.py
+++ src/service.py
@@ -8,26 +8,55 @@ app = Flask(__name__)
 server = Server()
 
 
-def token_required(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        code = server.process_token(request.headers, request.remote_addr)
-        if code != 200:
-            abort(code)
-
-        return f(*args, **kwargs)
-
-    return decorated
-
-
 @app.route("/signin", methods=['POST'])
 def signin():
-    return server.handle_signin(request.get_json(),request.remote_addr)
+    return server.handle_signin(request.get_json(), request.remote_addr)
+
 
 @app.route("/signup", methods=['POST'])
 def signup():
-    return server.signup(request.get_json(),request.remote_addr)
+    return server.handle_sign_up(request.get_json(), request.remote_addr)
 
+
+@app.route("/upload", methods=['POST'])
+def upload_video():
+    return server.handle_upload(request.get_data(), dict(request.headers), request.remote_addr)
+
+
+@app.route("/video/d/<string:hpath>")
+def get_video(hpath: str):
+    response, status = server.get_video(hpath)
+
+    if status != 200:
+        return response, status
+
+    return send_file(response["response"])
+
+
+@app.route("/video/<string:hpath>")
+def get_video_info(hpath: str):
+    return server.get_vinfo(hpath)
+
+
+@app.route("/videos/<string:rtype>", methods=['GET'])
+@app.route("/videos/<string:rtype>/<string:name>", methods=['GET'])
+def get_videos(rtype: str, name: str | None = None):
+    return server.retrieve_video_info(rtype, name)
+
+
+@app.route("/thumbnails/<string:hpath>", methods=['GET'])
+def get_thumbnails(hpath: str):
+    url, response = server.get_thumbnail_path(hpath)
+
+    if response != 200:
+        return url, response
+
+    return send_file(url["response"])
+
+
+@app.route("/search/<string:stype>/<string:squery>")
+def get_search_result(stype: str, squery: str):
+    return server.handle_search(squery, stype)
 
 
 if __name__ == "__main__":
